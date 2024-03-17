2d966995108a8f376b18f6f8437526307cc68fde
00012429 <saidlanibesti@gmail.com>
2024-03-17 21:14:43 +0500

    Controllers

---
+++ WAD.CW1.00012429/Controllers/MoviesController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._00012429.Dtos;
+using WAD.CW1._00012429.Models;
+using WAD.CW1._00012429.Services;
+
+namespace WAD.CW1._00012429.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MovieController : ControllerBase
+	{
+		private readonly IMovieRepository _movieRepository;
+		private readonly IMapper _mapper;
+
+		public MovieController(IMovieRepository movieRepository, IMapper mapper)
+		{
+			_movieRepository = movieRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<MovieDto>> GetAll()
+		{
+			var movies = _movieRepository.GetAll();
+			var movieDtos = _mapper.Map<IEnumerable<MovieDto>>(movies);
+			return Ok(movieDtos);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<MovieDto> Get(int id)
+		{
+			var movie = _movieRepository.GetById(id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
+
+			var movieDto = _mapper.Map<MovieDto>(movie);
+			return Ok(movieDto);
+		}
+
+		[HttpPost]
+		public ActionResult<MovieDto> Create([FromBody] MovieDto movieDto)
+		{
+			var movie = _mapper.Map<Movie>(movieDto);
+			_movieRepository.Add(movie);
+			return Ok(movieDto);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(int id, [FromBody] MovieDto movieDto)
+		{
+			var existingMovie = _movieRepository.GetById(id);
+			if (existingMovie == null)
+			{
+				return NotFound();
+			}
+
+			_mapper.Map(movieDto, existingMovie);
+			_movieRepository.Update(existingMovie);
+			return Ok("Updated");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var movie = _movieRepository.GetById(id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
+
+			_movieRepository.Delete(id);
+			return Ok("Deleted");
+		}
+	}
+}
---
+++ WAD.CW1.00012429/Controllers/ReviewsController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._00012429.Dtos;
+using WAD.CW1._00012429.Models;
+using WAD.CW1._00012429.Services;
+
+namespace WAD.CW1._00012429.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ReviewController : ControllerBase
+	{
+		private readonly IReviewRepository _reviewRepository;
+		private readonly IMapper _mapper;
+
+		public ReviewController(IReviewRepository reviewRepository, IMapper mapper)
+		{
+			_reviewRepository = reviewRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet("{movieId}")]
+		public ActionResult<IEnumerable<ReviewDto>> GetByMovieId(int movieId)
+		{
+			var reviews = _reviewRepository.GetByMovieId(movieId);
+			if (reviews == null)
+			{
+				return NotFound();
+			}
+
+			var reviewDtos = _mapper.Map<IEnumerable<ReviewDto>>(reviews);
+			return Ok(reviewDtos);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<ReviewDto> Get(int id)
+		{
+			var review = _reviewRepository.GetById(id);
+			if (review == null)
+			{
+				return NotFound();
+			}
+
+			var reviewDto = _mapper.Map<ReviewDto>(review);
+			return Ok(reviewDto);
+		}
+
+		[HttpPost]
+		public ActionResult<ReviewDto> Create([FromBody] ReviewDto reviewDto)
+		{
+			var review = _mapper.Map<Review>(reviewDto);
+			_reviewRepository.Add(review);
+			return Ok(reviewDto);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(int id, [FromBody] ReviewDto reviewDto)
+		{
+			var existingReview = _reviewRepository.GetById(id);
+			if (existingReview == null)
+			{
+				return NotFound();
+			}
+
+			_mapper.Map(reviewDto, existingReview);
+			_reviewRepository.Update(existingReview);
+			return Ok("Updated");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var review = _reviewRepository.GetById(id);
+			if (review == null)
+			{
+				return Ok("Deleted");
+			}
+
+			_reviewRepository.Delete(id);
+			return NoContent();
+		}
+	}
+}
